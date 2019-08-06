ab883a0deb0ef0e90e15681b1d7e280e32fe1d1e
Samuel Owusu <kwadwoamo10@gmail.com>
2019-08-06 21:29:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -83,8 +83,8 @@
 
 
 
-# def introduction(name)
-#  puts "Hi, my name is #{name}."
+# def introduction(samuel)
+#  puts "Hi, my name is #{samuel}."
 #end
 
 #def introduction_with_language(name, language)
